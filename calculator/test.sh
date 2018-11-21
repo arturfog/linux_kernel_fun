9167ae5769c46d8680f116268b621a5acdb3edb1
Artur Fogiel <arturprojekty1988@gmail.com>
2018-11-21 17:40:16 +0100

    Fixed copy paste mistake

--- calculator/test.sh
+++ calculator/test.sh
@@ -5,19 +5,19 @@ make
 sudo insmod calc.ko
 sudo chmod 0666 /dev/calc
 # testing multiply
-echo "Testing div: 5*5"
+echo "Testing: 5*5"
 echo -n '5*5\0' > /dev/calc
 cat /dev/calc
 # testing add
-echo "Testing div: 256+5"
+echo "Testing: 256+5"
 echo -n '256+5\0' > /dev/calc
 cat /dev/calc
 # testing sub
-echo "Testing div: 512-5"
+echo "Testing: 512-5"
 echo -n '512-5\0' > /dev/calc
 cat /dev/calc
 # testing div
-echo "Testing div: 25/5"
+echo "Testing: 25/5"
 echo -n '25/5\0' > /dev/calc
 cat /dev/calc
 sudo rmmod calc
